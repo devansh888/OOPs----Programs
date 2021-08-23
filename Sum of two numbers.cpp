140193931397a56162ccb4930d413e4b78cd7ab6
devansh888 <84080583+devansh888@users.noreply.github.com>
2021-08-23 19:02:46 +0530

    Add files via upload

---
+++ Sum of two numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first, second, sum;
+    
+    cout << "Enter two integers: ";
+    cin >> first >> second;
+
+    
+    sum = first + second;
+
+    
+    cout << first << " + " <<  second << " = " << sum;     
+
+    return 0;
+}
